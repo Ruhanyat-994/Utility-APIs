1c282c42bb261b7e6723e49e92a266c27adbd64f
Ruhanyat-994 <alruhanyat994@gmail.com>
2025-08-10 20:35:21 +0600

    Added A Simple UI using Mustache template !

--- Quotes-Generation/src/main/java/com/quotes/Quotes/Generation/controller/QuotesController.java
+++ Quotes-Generation/src/main/java/com/quotes/Quotes/Generation/controller/QuotesController.java
@@ -5,31 +5,36 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quotes.Quotes.Generation.entity.Quotes;
 import jakarta.annotation.PostConstruct;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Random;
 
-@RestController
+@Controller
 public class QuotesController {
     private List<Quotes> quotes;
     private final Random random = new Random();
 
     @PostConstruct
-    public void init() throws IOException{
+    public void init() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         InputStream inputStream = new ClassPathResource("quotes.json").getInputStream();
         quotes = mapper.readValue(inputStream, new TypeReference<List<Quotes>>() {});
     }
+
+    @GetMapping("/")
+    public String homePage() {
+        return "quotes"; // This should map to quotes.mustache template
+    }
+
     @GetMapping("/quote")
-    public Quotes getQuotes(){
+    @ResponseBody
+    public Quotes getRandomQuote() {
         int index = random.nextInt(quotes.size());
         return quotes.get(index);
     }
-
-
-
 }
