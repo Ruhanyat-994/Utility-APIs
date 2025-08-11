bc6519fef2b15069748d919722ddb9afe9f002e8
Ruhanyat-994 <alruhanyat994@gmail.com>
2025-08-11 13:33:45 +0600

    Initial URL Shortner Project Added!

---
+++ URL-SHORTER/src/main/java/com/linkshortslinks/URL_SHORTER/entity/urlEntity.java
@@ -0,0 +1,11 @@
+package com.linkshortslinks.URL_SHORTER.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class urlEntity {
+}
